5cd548b27e502d70b0ad8db3c035f675db2de3fc
Batuhantuncdev <76177986+batuhantuncdev@users.noreply.github.com>
2021-06-07 15:23:40 +0300

    merge

--- src/main/java/Ornek.java
+++ src/main/java/Ornek.java
@@ -2,5 +2,17 @@
 public class Ornek {
 	private String isim = "ali";
 	private String soyisim = "mert";
-  private String ad = "yigit";
+    private String ad = "yigit";
+    private String ad1 = "batuhan";
+  
+  
+  
+  
+  
+  
+  
+  
 }
+
+
+
